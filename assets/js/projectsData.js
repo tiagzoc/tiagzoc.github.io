c6e19bb5b9fdde85bcbae15f3a2bca90bcae6a39
BipBip <tzocca@gaming.tech>
2026-05-04 01:47:25 +0200

    [UPDATE] ProjectsData

--- assets/js/projectsData.js
+++ assets/js/projectsData.js
@@ -1,14 +1,95 @@
 const contributorsMap = {
   "HVannierM": "https://github.com/HVannierM",
-  "Kossouurio": "https://github.com/Kossouurio"
+  "Kossouurio": "https://github.com/Kossouurio",
+  "MithanielVillard": "https://github.com/MithanielVillard",
+  "REAPERtmm": "https://github.com/REAPERtmm",
+  "Paul-Henri-Solat-Gtech": "https://github.com/Paul-Henri-Solat-Gtech",
+  "Skaro971": "https://github.com/Skaro971",
+  "Kikii95": "https://github.com/Kikii95",
+  "fredanie2005": "https://github.com/fredanie2005",
+  "SidneyPlt": "https://github.com/SidneyPlt",
 };
 
 const projectsData = {
-  "AboveTale": {
+  "AboveTale":
+   {
     languages: ["C#", "Console", "JSON"],
-    apis: ["OpenGL"],
-    libraries: ["GLM"],
+    apis: [],
+    libraries: ["Newtonsoft"],
     collaborators: ["HVannierM", "Kossouurio"],
     media: "/assets/images/game-engine.png"
-  }
+  },
+
+  "AssetEngine":
+   {
+    languages: ["C++", "Console"],
+    apis: [],
+    libraries: ["Standard C++ library"],
+    collaborators: ["MithanielVillard"],
+    media: "/assets/images/game-engine.png"
+  },
+
+    "AudioEngine":
+   {
+    languages: ["C++"],
+    apis: ["SFML"],
+    libraries: ["XAudio2"],
+    collaborators: ["REAPERtmm"],
+    media: "/assets/images/game-engine.png"
+  },
+
+    "DonutMath":
+   {
+    languages: ["C++", "Console"],
+    apis: [],
+    libraries: [],
+    collaborators: [],
+    media: "/assets/images/game-engine.png"
+  },
+
+    "FBX-OBJ-Parser":
+   {
+    languages: ["C++"],
+    apis: ["OpenGL", "glad"],
+    libraries: ["GLM"],
+    collaborators: ["Paul-Henri-Solat-Gtech"],
+    media: "/assets/images/game-engine.png"
+  },
+
+    "MultiPlayer":
+   {
+    languages: ["C++"],
+    apis: ["SFML"],
+    libraries: ["Winsock2"],
+    collaborators: ["Skaro971"],
+    media: "/assets/images/game-engine.png"
+  },
+
+    "MultiThreading":
+   {
+    languages: ["C++"],
+    apis: ["SFML"],
+    libraries: ["Windows native library"],
+    collaborators: ["Kikii95", "Skaro971"],
+    media: "/assets/images/game-engine.png"
+  },
+
+    "PIT_Engine":
+   {
+    languages: ["C++"],
+    apis: ["DirectX12"],
+    libraries: [],
+    collaborators: ["REAPERtmm", "Skaro971"],
+    media: "/assets/images/game-engine.png"
+  },
+
+    "Shoot-Em-Up":
+   {
+    languages: ["C++"],
+    apis: ["SFML"],
+    libraries: [],
+    collaborators: ["fredanie2005", "SidneyPlt"],
+    media: "/assets/images/game-engine.png"
+  },
+
 };
